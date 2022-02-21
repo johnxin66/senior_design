486fbc3a07a4d95a1ad037e6d8525bd62641d84b
John Xin <54964822+johnxin66@users.noreply.github.com>
2022-02-21 08:50:43 +0800

    mock flight completed successfuly

--- source/fly.py
+++ source/fly.py
@@ -6,6 +6,8 @@ import tempfile
 import pprint
 import cv2
 
+_WAY_POINTS = [[0, 0, -30], [15, 15, -30], [20, 15, -30], [20, 20, -30], [25, 25, -30], [40, -30, -30], [45, 40, -30], [50, 50, -30], [50, 50, -5]]
+
 # connect to the AirSim simulator
 client = airsim.MultirotorClient()
 client.confirmConnection()
@@ -39,46 +41,47 @@ client.takeoffAsync().join()
 state = client.getMultirotorState()
 print("state: %s" % pprint.pformat(state))
 
-airsim.wait_key('Press any key to move vehicle to (-10, 10, -10) at 5 m/s')
-client.moveToPositionAsync(-10, 10, -10, 5).join()
+airsim.wait_key('Press any key to move vehicle to the destination at 5 m/s')
+for waypoint in _WAY_POINTS:
+    client.moveToPositionAsync(waypoint[0], waypoint[1], waypoint[2], 5).join()
 
 client.hoverAsync().join()
 
 state = client.getMultirotorState()
 print("state: %s" % pprint.pformat(state))
 
-airsim.wait_key('Press any key to take images')
-# get camera images from the car
-responses = client.simGetImages([
-    airsim.ImageRequest("0", airsim.ImageType.DepthVis),  #depth visualization image
-    airsim.ImageRequest("1", airsim.ImageType.DepthPerspective, True), #depth in perspective projection
-    airsim.ImageRequest("1", airsim.ImageType.Scene), #scene vision image in png format
-    airsim.ImageRequest("1", airsim.ImageType.Scene, False, False)])  #scene vision image in uncompressed RGBA array
-print('Retrieved images: %d' % len(responses))
+# airsim.wait_key('Press any key to take images')
+# # get camera images from the car
+# responses = client.simGetImages([
+#     airsim.ImageRequest("0", airsim.ImageType.DepthVis),  #depth visualization image
+#     airsim.ImageRequest("1", airsim.ImageType.DepthPerspective, True), #depth in perspective projection
+#     airsim.ImageRequest("1", airsim.ImageType.Scene), #scene vision image in png format
+#     airsim.ImageRequest("1", airsim.ImageType.Scene, False, False)])  #scene vision image in uncompressed RGBA array
+# print('Retrieved images: %d' % len(responses))
 
-tmp_dir = os.path.join(tempfile.gettempdir(), "airsim_drone")
-print ("Saving images to %s" % tmp_dir)
-try:
-    os.makedirs(tmp_dir)
-except OSError:
-    if not os.path.isdir(tmp_dir):
-        raise
+# tmp_dir = os.path.join(tempfile.gettempdir(), "airsim_drone")
+# print ("Saving images to %s" % tmp_dir)
+# try:
+#     os.makedirs(tmp_dir)
+# except OSError:
+#     if not os.path.isdir(tmp_dir):
+#         raise
 
-for idx, response in enumerate(responses):
+# for idx, response in enumerate(responses):
 
-    filename = os.path.join(tmp_dir, str(idx))
+#     filename = os.path.join(tmp_dir, str(idx))
 
-    if response.pixels_as_float:
-        print("Type %d, size %d" % (response.image_type, len(response.image_data_float)))
-        airsim.write_pfm(os.path.normpath(filename + '.pfm'), airsim.get_pfm_array(response))
-    elif response.compress: #png format
-        print("Type %d, size %d" % (response.image_type, len(response.image_data_uint8)))
-        airsim.write_file(os.path.normpath(filename + '.png'), response.image_data_uint8)
-    else: #uncompressed array
-        print("Type %d, size %d" % (response.image_type, len(response.image_data_uint8)))
-        img1d = np.fromstring(response.image_data_uint8, dtype=np.uint8) # get numpy array
-        img_rgb = img1d.reshape(response.height, response.width, 3) # reshape array to 4 channel image array H X W X 3
-        cv2.imwrite(os.path.normpath(filename + '.png'), img_rgb) # write to png
+#     if response.pixels_as_float:
+#         print("Type %d, size %d" % (response.image_type, len(response.image_data_float)))
+#         airsim.write_pfm(os.path.normpath(filename + '.pfm'), airsim.get_pfm_array(response))
+#     elif response.compress: #png format
+#         print("Type %d, size %d" % (response.image_type, len(response.image_data_uint8)))
+#         airsim.write_file(os.path.normpath(filename + '.png'), response.image_data_uint8)
+#     else: #uncompressed array
+#         print("Type %d, size %d" % (response.image_type, len(response.image_data_uint8)))
+#         img1d = np.fromstring(response.image_data_uint8, dtype=np.uint8) # get numpy array
+#         img_rgb = img1d.reshape(response.height, response.width, 3) # reshape array to 4 channel image array H X W X 3
+#         cv2.imwrite(os.path.normpath(filename + '.png'), img_rgb) # write to png
 
 airsim.wait_key('Press any key to reset to original state')
 
